231e2b631806c9a34e9f20706b339639a7366cff
Kazue Kurosaki <137025030+kazuekurosaki@users.noreply.github.com>
2026-02-13 21:01:50 +0700

    Add meta.php for site configuration and SEO

---
+++ meta.php
@@ -0,0 +1,20 @@
+<?php
+// meta.php
+$site_name = "NovelOnline - Baca Novel Gratis";
+$site_description = "Baca novel online gratis terbaru dan terpopuler. Koleksi lengkap novel Indonesia, terjemahan, dan update setiap hari.";
+$site_keywords = "novel online, baca novel gratis, novel terbaru, novel indonesia, novel terjemahan, baca novel web, novel update";
+$site_url = "https://domainanda.com";
+$site_logo = "assets/images/logo.png";
+$site_favicon = "assets/images/favicon.ico";
+$author = "NovelOnline Team";
+$copyright = "NovelOnline " . date('Y');
+
+// Social Media
+$facebook_url = "https://facebook.com/novelonline";
+$twitter_url = "https://twitter.com/novelonline";
+$instagram_url = "https://instagram.com/novelonline";
+
+// SEO Settings
+$enable_seo = true;
+$google_analytics_id = "UA-XXXXX-Y"; // Ganti dengan ID GA Anda
+?>
